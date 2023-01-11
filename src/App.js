9e654a6a3d5acbb139937720b3a47fce4d3a1440
Tapendra18 <107476177+Tapendra18@users.noreply.github.com>
2023-01-11 19:19:06 +0530

    cart buynow component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import Maincomp from './components/home/Maincomp';
 import Footer from './components/footer/Footer';
 import SignUp from './components/signup_sign/SignUp';
 import Sign_in from './components/signup_sign/Sign_in';
-import {Routes , Route} from 'react-router-dom'
+import {Routes , Route} from 'react-router-dom';
+import Cart from './components/cart/Cart';
+import Buynow from './components/buynow/Buynow';
 
 
 function App() {
@@ -17,6 +19,8 @@ function App() {
       <Route path='/' element={<Maincomp/>} />
       <Route path='/login' element={<Sign_in/>} />
       <Route path='/register' element={<SignUp/>} />
+      <Route path='/getproductsone/:id' element={<Cart/>} />
+      <Route path='/buynow' element={<Buynow/>}/>
       </Routes>
       <Footer/>
     </div>
